a3bb98e482e202aba57e0efd848e1b4792e0f3de
Jean Sébastien FRANTZ <rougeterrien@gmail.com>
2019-01-04 15:42:09 +0100

    Initial Commit

---
+++ Rick et Morty API/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rick et Morty API
+//
+//  Created by seb on 04/01/2019.
+//  Copyright © 2019 seb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
